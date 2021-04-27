22c9eb53c0707aabac1db314ad5d6e4c5eb4e180
harrychester <40340209+harrychester@users.noreply.github.com>
2021-04-28 00:25:51 +0100

    added db init, making of the network and volume

--- scripts/one-off-run-config-script.sh
+++ scripts/one-off-run-config-script.sh
@@ -1,1 +1,12 @@
+docker network create --subnet=203.0.113.0/24 kilo/iss_2021_n
+
+docker volume create kilo_database
+
+docker run -d -v kilo_database:/var/lib/mysql:rw --cap-drop ALL --cap-add CAP_DAC_READ_SEARCH --cap-add CAP_SETGID --cap-add CAP_SETUID  --security-opt label:type:kilo_db_t --net kilo/iss_2021_n --ip 203.0.113.88 --hostname db.cyber.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="iss_db" --name kilo_iss_2021-db_c kilo/iss_2021-db_i 
+
+docker exec -i kilo_iss_2021-db_c mysql -uroot -pCorrectHorseBatteryStaple < dbserver/sqlconfig/iss_db.sql
+
+docker stop kilo_iss_2021-db_c
+
+docker rm kilo_iss_2021-db_c
 
