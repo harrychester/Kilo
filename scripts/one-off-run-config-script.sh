98802713250d4760d67440e0cf752d5636658105
harrychester <40340209+harrychester@users.noreply.github.com>
2021-04-28 01:07:09 +0100

    added selinux lines

--- scripts/one-off-run-config-script.sh
+++ scripts/one-off-run-config-script.sh
@@ -2,6 +2,10 @@ docker network create --subnet=203.0.113.0/24 kilo/iss_2021_n
 
 docker volume create kilo_database
 
+cd ../builds/web; sudo make -f /usr/share/selinux/devel/Makefile kilo_web.pp && sudo semodule -i kilo_web.pp
+
+cd ../db; sudo make -f /usr/share/selinux/devel/Makefile kilo_db.pp && sudo semodule -i kilo_db.pp
+
 docker run -d -v kilo_database:/var/lib/mysql:rw --cap-drop ALL --cap-add CAP_DAC_READ_SEARCH --cap-add CAP_SETGID --cap-add CAP_SETUID  --security-opt label:type:kilo_db_t --net kilo/iss_2021_n --ip 203.0.113.88 --hostname db.cyber.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="iss_db" --name kilo_iss_2021-db_c kilo/iss_2021-db_i 
 
 docker exec -i kilo_iss_2021-db_c mysql -uroot -pCorrectHorseBatteryStaple < dbserver/sqlconfig/iss_db.sql
